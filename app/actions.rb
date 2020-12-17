4869597e424d3033402dc9ca248dcea797af4601
sashamahalia <sasha.camila.mahalia@gmail.com>
2020-12-17 00:57:28 +0000

    changes for deployment

--- app/actions.rb
+++ app/actions.rb
@@ -62,3 +62,58 @@ get '/logout' do
     session[:user_id] = nil
     redirect to('/')
 end
+
+before '/finstagram_posts/new' do
+    if !current_user
+        redirect('/login')
+    end
+end
+
+get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+    photo_url = params[:photo_url]
+
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+    if @finstagram_post.save
+        redirect(to ('/'))
+    else
+        erb(:"finstagram_posts/new")
+    end
+end
+
+get '/finstagram_posts/:id' do
+    @finstagram_post = FinstagramPost.find(params[:id])
+    erb(:"finstagram_posts/show")
+end
+
+post '/comments' do
+    text = params[:text]
+    finstagram_post_id = params[:finstagram_post_id]
+
+    comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+    comment.save
+
+    redirect(back)
+end
+
+post '/likes' do
+    finstagram_post_id = params[:finstagram_post_id]
+
+    like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+    like.save
+
+    redirect(back)
+end
+
+delete '/likes/:id' do
+    like = Like.find(params[:id])
+    like.destroy
+    redirect(back)
+end
